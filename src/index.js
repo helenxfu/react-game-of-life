b17d8b78b1614a31dbfbb49666a18efbf35ec3f2
helenxfu <helenxfubiz@gmail.com>
2019-06-17 10:43:42 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,201 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  };
+  render() {
+    return (
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    );
+  }
+}
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 16;
+    var rowsArr = [];
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Buttons extends React.Component {
+  handleSelect = event => {
+    this.props.gridSize(event.target.value);
+  };
+  render() {
+    return (
+      <div className="center">
+        <button onClick={this.props.playButton}>play</button>
+        <button onClick={this.props.pauseButton}>pause</button>
+        <button onClick={this.props.clear}>clear</button>
+        <button onClick={this.props.slow}>slow</button>
+        <button onClick={this.props.fast}>fast</button>
+        <button onClick={this.props.seed}>seed</button>
+        <select id="size-menu" onChange={this.handleSelect}>
+          <option value="1">20x10</option>
+          <option value="2">50x30</option>
+          <option value="3">70x50</option>
+        </select>
+      </div>
+    );
+  }
+}
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false))
+    };
+  }
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({ gridFull: gridCopy });
+  };
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (Math.floor(Math.random() * 4) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({ gridFull: gridCopy });
+  };
+
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  };
+
+  pauseButton = () => {
+    clearInterval(this.intervalId);
+  };
+
+  slow = () => {
+    this.speed = 1000;
+    this.playButton();
+  };
+  fast = () => {
+    this.speed = 100;
+    this.playButton();
+  };
+  clear = () => {
+    clearInterval(this.intervalId);
+    var grid = Array(this.rows)
+      .fill()
+      .map(() => Array(this.cols).fill(false));
+    this.setState({ gridFull: grid, generation: 0 });
+  };
+  gridSize = size => {
+    console.log(size);
+    switch (size) {
+      case "1":
+        this.rows = 10;
+        this.cols = 20;
+        break;
+      case "2":
+        this.rows = 30;
+        this.cols = 50;
+        break;
+      default:
+        this.rows = 50;
+        this.cols = 70;
+    }
+    this.clear();
+  };
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && j < this.cols - 1)
+          if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1
+    });
+  };
+
+  componentDidMount() {
+    this.seed();
+    this.playButton();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Game of Life</h1>
+        <Buttons
+          playButton={this.playButton}
+          pauseButton={this.pauseButton}
+          slow={this.slow}
+          fast={this.fast}
+          clear={this.clear}
+          seed={this.seed}
+          gridSize={this.gridSize}
+        />
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
